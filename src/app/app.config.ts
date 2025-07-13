e1aca3e47c8ca04b8542bad4b96b71b606e762dd
Ahmed Atef <Ahmedatef32003@gmail.com>
2025-07-13 18:07:22 +0300

    remove defult dark mode primng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -19,6 +19,10 @@ export const appConfig: ApplicationConfig = {
     providePrimeNG({
       theme: {
         preset: Aura,
+        
+        options: {
+            darkModeSelector: '.my-app-dark'
+        }
       },
     }),
   ],

1b6d842e11ebc3c987e93fc37984e08c70f31b86
Ahmed Atef <Ahmedatef32003@gmail.com>
2026-07-08 13:06:41 +0300

    Refactor code structure for improved readability and maintainability

--- src/app/shared/components/ui/works-and-projects/works-and-projects.component.ts
+++ src/app/shared/components/ui/works-and-projects/works-and-projects.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, PLATFORM_ID } from '@angular/core';
-import { ProjectCardComponent } from "../project-card/project-card.component";
+import { ProjectCardComponent } from '../project-card/project-card.component';
 import { IProject } from '../../../interfaces/project';
 import { isPlatformBrowser } from '@angular/common';
 import AOS from 'aos';
@@ -8,11 +8,10 @@ import AOS from 'aos';
   selector: 'app-works-and-projects',
   imports: [ProjectCardComponent],
   templateUrl: './works-and-projects.component.html',
-  styleUrl: './works-and-projects.component.css'
+  styleUrl: './works-and-projects.component.css',
 })
 export class WorksAndProjectsComponent {
-
-    private _PLATFORM_ID = inject(PLATFORM_ID);
+  private _PLATFORM_ID = inject(PLATFORM_ID);
   ngOnInit() {
     if (isPlatformBrowser(this._PLATFORM_ID)) {
       AOS.init({
@@ -22,44 +21,43 @@ export class WorksAndProjectsComponent {
     }
   }
 
-projects:IProject[]=[
+  projects: IProject[] = [
+    {
+      name: 'Egyptian Authority for Maritime Safety',
+      image: 'images/projects/EAMS.png',
+      link: 'https://www.eams.gov.eg/#/home',
+    },
+    {
+      name: 'Movie App',
+      image: 'images/projects/Movie App.png',
+      link: 'https://movie-app-nu-topaz.vercel.app/',
+    },
+    {
+      name: 'e-commerce',
+      image: 'images/projects/e-commers.png',
+      link: 'https://e-commerce-fresh-cart-rho.vercel.app/',
+    },
+    {
+      name: 'Social Media App',
+      image: 'images/projects/Social App.png',
+      link: 'https://soical-app.vercel.app/',
+    },
+    {
+      name: 'Note App',
+      image: 'images/projects/Note App.png',
+      link: 'https://note-app-ten-livid.vercel.app/',
+    },
 
-  {
-    name:"e-commerce",
-    image:"images/projects/e-commers.png",
-    link:"https://e-commerce-fresh-cart-rho.vercel.app/"
-  },
-  {
-    name:"Social Media App",
-    image:"images/projects/Social App.png",
-    link:"https://soical-app.vercel.app/"
-  },
-  {
-    name:"Note App",
-    image:"images/projects/Note App.png",
-    link:"https://note-app-ten-livid.vercel.app/"
-  },
+    {
+      name: 'Quiz App',
+      image: 'images/projects/Quiz App.png',
+      link: 'https://quiz-app-rho-ten-42.vercel.app/',
+    },
 
-  {
-    name:"Quiz App",
-    image:"images/projects/Quiz App.png",
-    link:"https://quiz-app-rho-ten-42.vercel.app/"
-  },
-
-
-  {
-    name:"Recipe App",
-    image:"images/projects/Resips.png",
-    link:"https://recipe-using-sass-7dnh.vercel.app/"
-  },
-
-   {
-    name:"Movie App",
-    image:"images/projects/Movie App.png",
-    link:"https://movie-app-nu-topaz.vercel.app/"
-  },
-
-]
-
-
+    // {
+    //   name: 'Recipe App',
+    //   image: 'images/projects/Resips.png',
+    //   link: 'https://recipe-using-sass-7dnh.vercel.app/',
+    // },
+  ];
 }
